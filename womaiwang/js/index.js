a85070157f74d32ed68e98ac007880f8aa952593
ss <ss>
2018-12-02 00:32:26 +0800

    第三次修改

--- womaiwang/js/index.js
+++ womaiwang/js/index.js
@@ -85,8 +85,8 @@ $(".navright .text").next("div").mouseenter(function() {
 	$(this).hide();;
 });
 //banner
-//定义选项卡方法
 $.fn.extend({
+	//定义选项卡方法
 	fnInit : function( obj, str1, str2 ){
 		this.mouseenter(function(){
 			/* Act on the event */
@@ -94,7 +94,40 @@ $.fn.extend({
 			var index = $(this).index();
 			$(obj).eq(index).addClass(str2).siblings().removeClass(str2);
 		});
+	},
+	//定义main中移动的方法
+	fnMainYD : function(obj1, obj2,obj3){
+		this.mouseenter(function(event) {
+			/* Act on the event */
+			$(obj1).show();
+		}).mouseleave(function(event) {
+			/* Act on the event */
+			$(obj1).hide();
+		}).on("click", ".jiao1Left", function(){
+			if($(obj2).position().left == 0){
+			$(obj2).animate({left: -$(obj2).width()});
+			}else if($(obj2).position().left == -$(obj3).width()){
+				$(obj2).animate({left: 0});
+			}
+		}).on("click", ".jiao1Right", function(){
+			if($(obj2).position().left == 0){
+				$(obj2).animate({left: -$(obj2).width()});
+			}else if($(obj2).position().left == -$(obj3).width()){
+				$(obj2).animate({left: 0});
+			}
+		})
+	},
+	//定义鲜艳效果的方法
+	fnxy:function(obj1){
+			this.mouseenter(function(event) {
+			/* Act on the event */
+			$(obj1).show().animate({left:242}, 800, function(){
+				$(obj1).hide().css({left:-242})
+			})
+		})
 	}
+	//小轮播
+	
 })
 //banner右边的小选项卡
 $(".bannerRightTop li").fnInit( ".bannerRightTop+ul li", "bannerRightTopOne", "displayblock" );
@@ -209,12 +242,39 @@ $(".dtwo li").mouseenter(function(event) {
 	this.div.remove();
 });
 //mainOne
-$(".mainbottomright ul").mouseenter(function(event) {
+$(".mainOne .mainbottomright").fnMainYD(".mainOne .jiao1 li", ".mainOne .mainbottomyd", ".mainOne .mainbottomright");
+$(".mainbottomright li a img").mouseenter(function(event) {
 	/* Act on the event */
-	$(".jiao1 li").show();
+	$(this).stop().animate({width:216,height:216}, 600)
 }).mouseleave(function(event) {
 	/* Act on the event */
-	$(".jiao1 li").hide();
-}).on("click", ".jiao1Left", function(){
-	$(".mainbottomright ul").animate({left: -$(".mainbottomright ul").width()})
-})
+	$(this).stop().animate({width:180,height:180}, 600)
+});
+$(".mainOne .bottomleft").fnxy(".mainOne .bottomleft .aImg")
+//mainThree
+$(".mainThree .mainThreebottom").fnMainYD(".mainThree .jiao1 li", ".mainThree .mainbottomyd",".mainThree .mainThreebottom");
+//floor选项卡
+$(".floorOne .floorRightTop li").fnInit(".floorOne .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorTwo .floorRightTop li").fnInit(".floorTwo .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorThree .floorRightTop li").fnInit(".floorThree .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorFour .floorRightTop li").fnInit(".floorFour .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorFive .floorRightTop li").fnInit(".floorFive .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorSix .floorRightTop li").fnInit(".floorSix .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorSeven .floorRightTop li").fnInit(".floorSeven .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorEight .floorRightTop li").fnInit(".floorEight .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorNine .floorRightTop li").fnInit(".floorNine .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorTen .floorRightTop li").fnInit(".floorTen .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+$(".floorEleven .floorRightTop li").fnInit(".floorEleven .floorRight>ul:last-child>li", "floorxxk", "displayblock");
+//floor鲜艳
+$(".floorOne .bottomleft").fnxy(".floorOne .bottomleft .aImg")
+$(".floorTwo .bottomleft").fnxy(".floorTwo .bottomleft .aImg")
+$(".floorThree .bottomleft").fnxy(".floorThree .bottomleft .aImg")
+$(".floorFour .bottomleft").fnxy(".floorFour .bottomleft .aImg")
+$(".floorFive .bottomleft").fnxy(".floorFive .bottomleft .aImg")
+$(".floorSix .bottomleft").fnxy(".floorSix .bottomleft .aImg")
+$(".floorSeven .bottomleft").fnxy(".floorSeven .bottomleft .aImg")
+$(".floorEight .bottomleft").fnxy(".floorEight .bottomleft .aImg")
+$(".floorNine .bottomleft").fnxy(".floorNine .bottomleft .aImg")
+$(".floorTen .bottomleft").fnxy(".floorTen .bottomleft .aImg")
+$(".floorEleven>div:first-child .bottomleft").fnxy(".floorEleven>div:first-child .bottomleft .aImg")
+$(".floorTwelve .bottomleft").fnxy(".floorTwelve .bottomleft .aImg")
